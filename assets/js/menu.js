2f574661e4d1b99f19e8b2242088ecbae8fe923d
FrannieBushi <163539808+FrannieBushi@users.noreply.github.com>
2025-01-13 17:48:03 +0100

    Update menu.js

--- assets/js/menu.js
+++ assets/js/menu.js
@@ -1,11 +1,11 @@
 document.addEventListener("DOMContentLoaded", function() {
-    // Seleccionamos el botón y el menú
+    
     const menuToggle = document.querySelector('.menu-toggle');
     const menu = document.getElementById('menu');
 
     // Función para mostrar u ocultar el menú
     function toggleMenu() {
-        // Cambiamos la visibilidad del menú
+        
         if (menu.style.display === 'none' || menu.style.display === '') {
             menu.style.display = 'block'; // Mostrar el menú
         } else {
@@ -13,6 +13,6 @@ document.addEventListener("DOMContentLoaded", function() {
         }
     }
 
-    // Asignamos el evento al botón
+    
     menuToggle.addEventListener('click', toggleMenu);
 });
